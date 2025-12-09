ceea4637bd67121b7aeb6ac31a6779e514991347
DDBao <dangdaibao94@gmail.com>
2025-12-09 21:05:36 +0700

    Thuc hanh buoi 3

---
+++ Thuc hanh buoi 3.py
@@ -0,0 +1,40 @@
+# bai tap 1
+name="Bao"
+age="20"
+hobby="games"
+print(name)
+print(age)
+print(hobby)
+
+# bai tap 2
+a = int(input("Nhap so dau tien: "))
+b = int(input("Nhap so thu hai: "))
+c = int(input("Nhap so thu ba: "))
+result = (a+b+c)/3
+print('So trung binh cong', result)
+
+# bai tap 3
+a = int(input("Nhap so don gia mat hang X: "))
+b = int(input("Nhap so so luong mat hang X: "))
+result = a*b
+print('Thanh tien', result)
+
+# bai tap 4
+a = int(input("Nhap so dau tien: "))
+b = int(input("Nhap so thu hai: "))
+c = int(input("Nhap so thu ba: "))
+result = a**2+b**2+c**3
+print('Ket qua bieu thuc a2+b2+c3', result)
+
+# bai tap 5
+a = int(input("Nhap so phut da goi: "))
+b = 20000 # thuê bao: 20,000
+c = 1000 # đơn giá: 1,000
+result = a*c+b
+print('So tien phai tra', result)
+
+# bai tap 6
+a = int(input("Nhap chieu dai hinh chu nhat (mét): "))
+b = int(input("Nhap chieu rong hinh chu nhat (mét): "))
+result = a*b
+print('Dien tich hinh chu nhat: ', result, 'm2')
