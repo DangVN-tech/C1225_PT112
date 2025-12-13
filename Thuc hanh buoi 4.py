7d08b3001b92b3e49455a6a95e50d3931e2af3e1
DDBao <dangdaibao94@gmail.com>
2025-12-13 20:31:25 +0700

    13122025

---
+++ Thuc hanh buoi 4.py
@@ -0,0 +1,35 @@
+# Bai tap 1
+#a = int(input("Nhap so a: "))
+#b = int(input("Nhap so b: "))
+#result = a**b
+#print("Ket qua cua a^b= ", result)
+
+# Bai tap 2
+#r = int(input("Nhap ban kinh hinh tron: "))
+#print("Chu vi hinh tron ", 2*3.14*r)
+#print("Dien tich hinh tron ", 3.14*r**2)
+
+# Bai tap 3
+#a = int(input("Nhap Tu so a: "))
+#b = int(input("Nhap Mau so b: "))
+#print("Bạn đã nhập phân số a/b có kết quả đổi ra số thực =", a/b)
+
+# Bai tap 4
+#C = int(input("Nhap Gia tri do C (Celsius): "))
+#print("Doi sang do F (Fahrenheit):", C*1.8+32)
+
+# Bai tap 6
+#x = int(input("Nhap so nguyen co 3 chu so:"))
+#print("Tong cac chu so la:", x=a+b+c)
+
+# Bai tap 7
+#a = int(input("Nhap so a: "))
+#b = int(input("Nhap so b: "))
+#print("Gia tri hoan doi a=", b)
+#print("Gia tri hoan doi b=", a)
+
+# Bai tap 8
+#a = float(input("Nhap so Tien gui: "))
+#b = int(input("Nhap so Nam gui: "))
+#c = float(input("Nhap Lai suat nam (%): "))
+#print("Tong tien ", a*(1 + c/100)**b)
---
+++ Thuc hanh buoi 5.py
@@ -0,0 +1,4 @@
+a = int(input("a= "))
+if (a%2 == 0):
+    print("so chan")
+print("so le")
