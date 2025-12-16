e9ec05649fe18ad6af09f00d8fc49478524bdacf
DDBao <dangdaibao94@gmail.com>
2025-12-16 21:19:58 +0700

    16122025

--- Thuc hanh buoi 4.py
+++ Thuc hanh buoi 4.py
@@ -13,20 +13,35 @@
 #a = int(input("Nhap Tu so a: "))
 #b = int(input("Nhap Mau so b: "))
 #print("Bạn đã nhập phân số a/b có kết quả đổi ra số thực =", a/b)
-
-# Bai tap 4
+#
+## Bai tap 4
 #C = int(input("Nhap Gia tri do C (Celsius): "))
 #print("Doi sang do F (Fahrenheit):", C*1.8+32)
 
+# Bai tap 5
+#x = int(input("Nhap so nguyen:"))
+#str_x = str(x)
+#fl_x = float(x)
+#bool_x = bool(x)
+#print(f"Kieu String: {str_x}")
+#print(f"Kieu Float {fl_x}")
+#print(f"Kieu Boolean: {bool_x}")
+
 # Bai tap 6
-#x = int(input("Nhap so nguyen co 3 chu so:"))
-#print("Tong cac chu so la:", x=a+b+c)
+#x = int(input("Nhap so nguyen co 3 chu so: "))
+#a = x//100
+#b = x//10-a*10
+#c = x-a*100-b*10 
+#print("Tong cac chu so la:", a+b+c)
 
 # Bai tap 7
-#a = int(input("Nhap so a: "))
-#b = int(input("Nhap so b: "))
-#print("Gia tri hoan doi a=", b)
-#print("Gia tri hoan doi b=", a)
+a = int(input("Nhap so a: "))
+b = int(input("Nhap so b: "))
+c = a
+b = a
+b = c
+print("Gia tri hoan doi a=", b)
+print("Gia tri hoan doi b=", a)
 
 # Bai tap 8
 #a = float(input("Nhap so Tien gui: "))
