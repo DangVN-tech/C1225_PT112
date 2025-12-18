01d207f4d5f1e39f0d4b2d9fff717455d67e55fd
DDBao <dangdaibao94@gmail.com>
2025-12-18 21:31:07 +0700

    18122025

---
+++ Thuc hanh buoi 7.py
@@ -0,0 +1,42 @@
+# Bai tap 1
+#count = 1
+#while (count<=200):
+#    print(count)
+#    count+=1
+#else:
+#    print("Ket thuc")
+
+# Bai tap 2
+#count = 200
+#while (count>=1):
+#    print(count)
+#    count-=1
+#else:
+#    print("Ket thuc")
+
+# Bai tap 3
+#a = int(input("Nhap vao 1 so nguyen duong: "))
+#count = 1
+#while (count<=a):
+#    if a%count==0:
+#        print("Uoc so gom: ", a/count)
+#    count+=1
+
+###### Bai tap 4
+#a = int(input("Nhap vao 1 so nguyen duong: "))
+#count = 0
+#while (count<=a):
+#    if count%3==0:
+#        print("So chia het cho 3 gom:", count)
+#    count+=1
+
+####### Bai tap 5
+#a = 1
+#while a!=0:
+#    a = int(input("Nhap vao so nguyen duong: "))
+#    if a%2==0:
+#            print("Day la so Chan")
+#    else:
+#            print("Day la so Le")
+#else:
+#    print("Ket thuc")
